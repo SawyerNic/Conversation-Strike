9f7464d5fec261eec007f658d89fdbb305d51eaa
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-04-28 21:20:48 -0400

    auto commit

--- client/home.jsx
+++ client/home.jsx
@@ -3,15 +3,21 @@ const { createRoot } = require('react-dom/client');
 const helper = require('./helper.js');
 const { Link } = require('react-router-dom');
 
-
 const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>
             <Link to="/loginPage">Go to Login</Link>
+
+            <div id="Feed">
+
+            </div>
         </div>
+        
     );
 }
+
+
 
 const init = () => {
     const root = createRoot(document.getElementById('content'));
